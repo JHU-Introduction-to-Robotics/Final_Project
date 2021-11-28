a6005a6ab45445f259ae83971fb9ec2fbd53f274
Sahil Sharma <sahil-sharma@outlook.com>
2021-11-28 12:47:22 -0500

    Initial Commit

---
+++ robot_control_pkg/launch/placeholder.launch.py
@@ -0,0 +1,15 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        Node(package='robot_control_pkg', executable='placeholder_control',
+         output='screen'),
+        Node(package='robot_control_pkg', executable='placeholder_estimator',
+        	 output='screen'),
+    ])
+
